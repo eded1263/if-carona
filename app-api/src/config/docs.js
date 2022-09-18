a3fb049bec0cc46dca2af24cab88b775ee9afab8
Edilson <edilson@quicktech.no>
2022-09-18 12:40:20 -0300

    feat: adicionado schema de aluno

--- app-api/src/config/docs.js
+++ app-api/src/config/docs.js
@@ -1,14 +1,14 @@
 const swaggerJsdoc = require("swagger-jsdoc");
 const opt = {
 	definition: {
-		swagger: "2.0",
+		openapi: "3.0.0",
 		info: {
 			title: "IF Carona API",
 			description: "API para a aplicação IF Carona",
 			version: process.env.NODE_ENV,
 		},
 	},
-	apis: ["**/*.controller.yaml"],
+	apis: ["**/*.controller.yaml", "**/*.scheme.yaml"],
 };
 
 const docsConfiguration = swaggerJsdoc(opt);
